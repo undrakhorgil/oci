d1a82ba3d593b818aa8e937dceddb83d285ccded
Undrakh Orgil Odonkhuu <uodonkhuu@miu.edu>
2025-09-28 14:14:56 -0500

    removed local config

--- provider.tf
+++ provider.tf
@@ -6,9 +6,9 @@ terraform {
     }
 }
 provider "oci" {
-    tenancy_ocid     = var.tenancy_ocid
-    user_ocid        = var.user_ocid
-    fingerprint      = var.fingerprint
-    private_key_path = var.private_key_path
+//    tenancy_ocid     = var.tenancy_ocid
+//    user_ocid        = var.user_ocid
+//    fingerprint      = var.fingerprint
+//    private_key_path = var.private_key_path
     region           = var.region
     }
